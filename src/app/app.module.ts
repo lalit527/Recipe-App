36d71375945bcb97a05bf50e63a382376191b24a
lalit-07 <lalit.slg007@gmail.com>
2017-11-06 01:44:57 +0530

    New

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ShoppingListComponent } from './shopping-list/list/shopping-list.component';
+import { ListEditComponent } from './shopping-list/edit/list-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ShoppingListComponent,
+    ListEditComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/shopping-list/edit/list-edit.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component ({
+    selector: 'edit-list',
+    templateUrl: './list-edit.component.html'
+})
+export class ListEditComponent {}
