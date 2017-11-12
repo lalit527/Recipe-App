4a2073ee4ff49b5e5db38cdfcd9965f69f44d6c4
lalit-07 <lalit.slg007@gmail.com>
2017-11-12 22:44:05 +0530

    Before Directive

--- src/app/shopping-list/edit/list-edit.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component ({
-    selector: 'edit-list',
-    templateUrl: './list-edit.component.html'
-})
-export class ListEditComponent {}
--- src/app/shopping-list/list/shopping-list.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'shopping-list',
-    templateUrl: './shopping-list.component.html'
-})
-export class ShoppingListComponent {}
